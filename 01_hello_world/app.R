7246f3ec8c154fc3ce1ff629191ccaefb9541aa9
Hadrien <hadrien@rstudio.com>
2020-06-10 20:34:40 +0000

    rename folders, import insurance app

---
+++ 01_hello_world/app.R
@@ -0,0 +1,18 @@
+# Build a shiny application from scratch
+
+# load the shiny package
+library(shiny)
+
+# define the UI + Server
+
+ui <- fluidPage(
+        # Application title
+        titlePanel("My application title")
+)
+
+server <- function(input, output) {
+    
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
---
+++ 02_grid_layout/app.R
@@ -0,0 +1,19 @@
+# Minimum viable example of a shiny app with a grid layout
+
+library(shiny)
+
+# Define UI
+ui <- fluidPage(
+  # Grid Layout
+    fluidRow(wellPanel("Top row")),
+    fluidRow(column(width = 4, wellPanel("Bottom row, column 1, width 4")),
+             column(width = 8, wellPanel("Bottom row, column 2, width 8"))))
+
+
+# Server function
+server <- function(input, output) {
+    
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
